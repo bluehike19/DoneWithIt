aff4e2d55dd4dcfe4f40fa720533194d57e66a58
simo blues <wambuisimon712@gmail.com>
2024-01-22 23:21:56 +0300

    changes

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello bluehike!</Text>
+      <Text numberOfLines={1} onPress={() => console.log("handle events")}>Hello bluehike!</Text>
       <StatusBar style="auto" />
     </View>
   );
